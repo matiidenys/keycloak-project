ceef4066a3913dd7bf21ddc94fc13a3a2654391d
Matii Denys <matiidenys@gmail.com>
2025-04-18 14:19:38 +0300

    lab 3

--- src/main/java/edu/pzks/keycloakproject/SecurityConfig.java
+++ src/main/java/edu/pzks/keycloakproject/SecurityConfig.java
@@ -1,20 +1,41 @@
 package edu.pzks.keycloakproject;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // Allow all requests without authentication
-                .csrf(AbstractHttpConfigurer::disable); // Disable CSRF protection (only if needed)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(req ->
+                        req.anyRequest().authenticated()
+                );
+        http
+                .oauth2ResourceServer( auth ->
+                        auth.jwt( jwt ->
+                                jwt.jwtAuthenticationConverter(new JwtAuthenticationConverter()))
+                );
+
+        http
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
 
         return http.build();
     }
+
 }
